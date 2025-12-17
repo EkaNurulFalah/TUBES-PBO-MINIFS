34a08debf48b2977604d12d398e9781f7fcd4492
nurmareko <nurmareko@yandex.ru>
2025-12-17 16:51:18 +0700

    Initial main class

--- application/Main.java
+++ application/Main.java
@@ -1,45 +1,11 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+package application;
+
+import application.classes.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Database credentials
-        String url = "jdbc:mysql://localhost:3306/minifs";
-        String user = "root";
-        String password = "";
-
-        try {
-            // Create connection
-            Connection conn = DriverManager.getConnection(url, user, password);
-
-            // Create statement
-            Statement stmt = conn.createStatement();
-
-            // Execute query
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users");
-
-            // Process results
-            while (rs.next()) {
-                System.out.println(
-                    rs.getInt("id") +
-                        " | " +
-                        rs.getString("username") +
-                        " | " +
-                        rs.getString("password") +
-                        " | " +
-                        rs.getString("role")
-                );
-            }
-
-            // Close resources
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Shell shell = new Shell();
+        shell.start();
     }
 }
